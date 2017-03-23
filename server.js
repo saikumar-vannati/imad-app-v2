dba89514a81af65fd791aa36d9dba0227df02fe1
saikumar-vannati <saikumar.vandati@gmail.com>
2017-03-23 19:05:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'saikumar-vannati',
+    database: 'saikumar-vannati',
+    host: 'db.imad.hasura-app.io',
+    password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -93,6 +101,22 @@ app.get('/counter',function(req,res){
 });
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db',function(req,res)
+{
+    //make a select request
+    //return a response with the results
+    pool.query('select * from test',function(err,result)
+    {
+       if(err){
+           res.status(500).send(err.toString());
+       }
+       else{
+           res.send(JSON.stringify(result));
+       }
+    });
 });
 
 app.get('/ui/style.css', function (req, res) {

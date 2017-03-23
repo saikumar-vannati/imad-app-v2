2564a2104b4fe4ac659407a2c36b46a2b5802f30
saikumar-vannati <saikumar.vandati@gmail.com>
2017-03-23 21:51:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -135,7 +135,7 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/articles/:articleName', function(req,res){
     // article name == article one
     // articles[articleName] =={} content object for article one
-    pool.query("select * from article where title = '"+ req.params.articleName "'",function(err, result){
+    pool.query("select * from article where title = '"+ req.params.articleName +"'" ,function(err, result){
         if(err){
             res.status(500).send(err.toString());
         }else{

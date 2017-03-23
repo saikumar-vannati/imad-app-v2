564461f1f6a4343f5db30ad35c688e03863c84b7
saikumar-vannati <saikumar.vandati@gmail.com>
2017-03-23 16:51:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,28 @@
-console.log('Loaded!');
-var element = document.getElementById('main_text');
+var button = document.getElementById('counter');
 
-element.innerHtml = 'this is saikumar and my first webapp';
-var img = document.getElementById('madi');
-
-var marginLeft= 0;
-
-function moveRight()
+button.onclick = function()
 {
-    marginLeft = marginLeft +5;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function(){
-    var interval = setInterval(moveRight, 50);
+    //create a request object
+    var request = new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    
+    request.onreadystatechange = function()
+    {
+        if(request.readyState ===XMLHttpRequest.DONE)
+        {
+            //take some action
+            if(request.status === 200)
+            {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        //not done yet
+    };
+    
+    //make the request
+    request.open('GET','http://saikumar-vannati.imad.hasura-app.io/counter',true);
+    request.send(null);
 };

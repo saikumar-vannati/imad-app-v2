e7226b51d3ac91030cd2ea07465b90956cb965b1
saikumar-vannati <saikumar.vandati@gmail.com>
2017-03-23 15:17:27 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -1,9 +1,11 @@
-@@ -59,7 +59,7 @@ function loadLogin () {
+function loadLogin () {
   
   function escapeHTML (text)
-  {
- -    var $text = document.createTextNode(html);
- +    var $text = document.createTextNode(text);
+    {
+     var $text = document.createTextNode(html);
+    var $text = document.createTextNode(text);
       var $div = document.createElement('div');
       $div.appendChild($text);
       return $div.innerHTML;
+    }
+}

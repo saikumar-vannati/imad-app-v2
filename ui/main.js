da8ddda008727bfd06035cdcaeb280c041e7b018
saikumar-vannati <saikumar.vandati@gmail.com>
2017-03-23 16:00:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,15 @@ console.log('Loaded!');
 var element = document.getElementById('main_text');
 
 element.innerHtml = 'this is saikumar and my first webapp';
+var img = document.getElementById('madi');
+
+var marginLeft= 0;
+
+function moveRight()
+{
+    marginLeft = marginLeft +5;
+    img.style.marginLeft = marginLeft + 'px';
+}
+img.onclick = function(){
+    var interval = setInterval(moveRight, 50);
+};
